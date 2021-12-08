60d0ac64be883c7ec02353c225ada130b952eaab
mira-kine <mirak0419@gmail.com>
2021-12-08 15:59:02 -0800

    added image and types to display

--- src/components/PokeList/PokeList.js
+++ src/components/PokeList/PokeList.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import './PokeList.css';
 // import { useState, useEffect } from 'react';
 
 export default function PokeList({ pokemon }) {
   return (
-    <div>
-      {pokemon.map((poke) => (
-        <p key={poke.id}>
-          {poke.pokemon}
-          {poke.type}
-        </p>
-      ))}
+    <div className="container">
+      <div className="poke-card">
+        {pokemon.map((poke) => (
+          <p key={poke.id}>
+            <span>{poke.pokemon}</span>
+            <span>{poke.type_1}</span>
+            <img src={poke.url_image} />
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
