be0e8c07a28b600d475f7786147235aa71521040
mira-kine <mirak0419@gmail.com>
2021-12-08 13:02:14 -0800

    updated userinput.js, span is displaying

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { getPokemon } from './services/pokemon';
 import { useEffect, useState } from 'react';
 import PokeList from './components/PokeList/PokeList';
+import UserInput from './components/UserInput/UserInput';
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
@@ -27,6 +28,7 @@ function App() {
       {loading && <span className="loader">!</span>}
       {!loading && (
         <>
+          <UserInput />
           <PokeList {...{ pokemon }} />
         </>
       )}
